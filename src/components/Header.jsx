149136ae7f41a9aad09597a8b6a74c6c6e5a7eac
SpiderWacho <iamgaas@gmail.com>
2023-02-21 17:26:37 -0300

    Add cart to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,31 @@
 import logo from '../img/logo-white.png'
-import cart from '../img/cart.png'
+import cartImg from '../img/cart.png'
 import './header.css'
 import { Link } from 'react-router-dom'
+import Cart from './Cart'
+import React, { useContext } from 'react';
+import { ProductsContext } from '../context/ProductsContext';
+import { CartContext } from '../context/CartContext'
 
-const Header = ({activateCart}) => {
+
+const Header = () => {
+    const {cart, setCart} = useContext(CartContext);
+      const {products} = useContext(ProductsContext);
+
 return (
-
-    <div className="header">
-    <span className='bar-img-container'>
-    <Link to='/'><img src={logo} className='logo'></img></Link>
-    <img src={cart} className='cart' onClick={activateCart}></img>
-    </span>
+    <div className='header-container'>
+        <div className="header">
+        <span className='logo-container'>
+            <Link to='/'><img src={logo} className='logo'></img></Link>
+        </span>
+        <span className='cart-container'>
+            <img src={cartImg} className='cart' onClick={() => setCart(true)}></img>
+            {products.length > 0 ? <div className='pop-up'>{products.length}</div> : null}
+        </span>
+        </div>
+        {cart ? <Cart products={products}/> : null} 
     </div>
+
 )
 }
 
